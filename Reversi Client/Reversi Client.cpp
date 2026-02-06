b5598b6bb8d4e7cbbbc7875b8a912595d53152c1
安達廉仁 <240333@E0403-37>
2026-02-06 16:27:33 +0900

    Clientの基本形完成

--- Reversi Client/Reversi Client.cpp
+++ Reversi Client/Reversi Client.cpp
@@ -8,7 +8,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
  //ウィンドウモードで起動
 	ChangeWindowMode(TRUE);
-	//Dxライブラリ初期化処理
+
+//Dxライブラリ初期化処理
 	DxLib_Init();
 
 	WSADATA wsa;
@@ -19,5 +20,42 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	sockaddr_in addr;
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(12345);
+	addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+
+	connect(sock, (sockaddr*)&addr, sizeof(addr));
+
+	Reversi game;
+
+	while (ProcessMessage() == 0)
+	{
+		char buf[16];
+		recv(sock, buf, sizeof(buf), 0);
+
+		int x, y;
+		sscanf(buf, "%d %d", &x, &y);
+		game.Put(x, y,1);
+		game.Draw();
+
+		if (GetMouseInput() & (MOUSE_INPUT_LEFT))
+		{
+			int mx, my;
+			GetMousePoint(&mx, &my);
+
+			x = (mx - 50) / 60;
+			y = (my - 50) / 60;
+
+			if (game.CanPut(x, y))
+			{
+				game.Put(x, y, 2);
+
+				sprintf_s(buf, "%d %d", x, y);
+				send(sock, buf, strlen(buf) + 1, 0);
+			}
+		}
+
+	}
+
+	DxLib_End();
+	return 0;
 
 }
