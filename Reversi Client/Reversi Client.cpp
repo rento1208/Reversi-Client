0222b38b19dc8b3dbe6429424a179ba4d7511df9
安達廉仁 <240333@E0403-37>
2026-02-06 14:02:41 +0900

    プロジェクト ファイルを追加します。

---
+++ Reversi Client/Reversi Client.cpp
@@ -0,0 +1,23 @@
+﻿#include <DxLib.h>
+#include <WinSock2.h>
+#include "Reversi.h"
+
+#pragma comment(lib, "ws2_32.lib")
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+ //ウィンドウモードで起動
+	ChangeWindowMode(TRUE);
+	//Dxライブラリ初期化処理
+	DxLib_Init();
+
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
+
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(12345);
+
+}
---
+++ Reversi Client/Reversi.cpp
@@ -0,0 +1,67 @@
+#include "Reversi.h"
+
+Reversi::Reversi()
+	: cellSize(60), Offset(50), board()
+{
+
+
+	for (int y = 0; y < 8; y++)
+	{
+		for (int x = 0; x < 8; x++)
+		{
+			board[y][x] = 0;
+			board[3][3] = 2;
+			board[3][4] = 1;
+			board[4][3] = 1;
+			board[4][4] = 2;
+		}
+	}
+}
+
+Reversi::~Reversi()
+{
+}
+
+void Reversi::Draw()
+{
+	ClearDrawScreen();
+	int Brack = GetColor(0, 0, 0);
+	int White = GetColor(255, 255, 255);
+	int Green = GetColor(0, 128, 0);
+
+	for (int y = 0; y < 8; y++)
+	{
+		for (int x = 0; x < 8; x++)
+		{
+			int drawX = Offset + x * CellSize;
+			int drawY = Offset + y * CellSize;
+
+			// ƒ}ƒX–Ú‚Ì•`‰æ
+			DrawBox(drawX, drawY, drawX + CellSize, drawY + CellSize, Green, TRUE);
+			DrawBox(drawX, drawY, drawX + CellSize, drawY + CellSize, Brack, FALSE);
+
+			// Î‚Ì•`‰æ
+			if (board[y][x] == 1)
+			{
+				DrawCircle(drawX + CellSize / 2, drawY + CellSize / 2, CellSize / 2 - 5, White, TRUE);
+			}
+			if (board[y][x] == 2)
+			{
+				DrawCircle(drawX + CellSize / 2, drawY + CellSize / 2, CellSize / 2 - 5, Brack, TRUE);
+			}
+		}
+	}
+
+	ScreenFlip();
+}
+
+bool Reversi::CanPut(int x, int y)
+{
+
+	return(x >= 0 && x < 8 && y >= 0 && y < 8 && board[y][x] == 0);
+}
+
+void Reversi::Put(int x, int y, int color)
+{
+	board[y][x] = color;
+}
